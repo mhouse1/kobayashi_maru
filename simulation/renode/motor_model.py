9f97b3f8805e584fa58e31c0cacc917bc78b16aa
mhouse_impulse <michaelhousera@gmail.com>
2025-12-07 01:38:00 -0500

    add moved files

---
+++ simulation/renode/motor_model.py
@@ -0,0 +1,103 @@
+"""
+Motor Model for Renode Simulation
+Simulates a brushless DC motor with encoder feedback for 4WD robot
+"""
+
+class MotorModel:
+    """
+    Motor simulation model for heavy duty 4WD robot wheels.
+    Supports CAN-FD communication for speed and position control.
+    """
+    
+    # Register offsets
+    REG_CONTROL = 0x00      # Control register (start/stop, direction)
+    REG_SPEED_SET = 0x04    # Speed setpoint (RPM)
+    REG_SPEED_ACT = 0x08    # Actual speed (RPM)
+    REG_POSITION = 0x0C     # Encoder position (ticks)
+    REG_CURRENT = 0x10      # Motor current (mA)
+    REG_STATUS = 0x14       # Status register
+    REG_CANFD_ID = 0x18     # CAN-FD node ID
+    
+    # Control bits
+    CTRL_ENABLE = 0x01
+    CTRL_DIRECTION = 0x02
+    CTRL_BRAKE = 0x04
+    
+    # Status bits
+    STATUS_RUNNING = 0x01
+    STATUS_FAULT = 0x02
+    STATUS_OVER_CURRENT = 0x04
+    STATUS_OVER_TEMP = 0x08
+    
+    def __init__(self):
+        self.control = 0
+        self.speed_setpoint = 0
+        self.actual_speed = 0
+        self.position = 0
+        self.current = 0
+        self.status = 0
+        self.canfd_id = 0x100
+        
+    def read(self, offset):
+        if offset == self.REG_CONTROL:
+            return self.control
+        elif offset == self.REG_SPEED_SET:
+            return self.speed_setpoint
+        elif offset == self.REG_SPEED_ACT:
+            return self.actual_speed
+        elif offset == self.REG_POSITION:
+            return self.position
+        elif offset == self.REG_CURRENT:
+            return self.current
+        elif offset == self.REG_STATUS:
+            return self.status
+        elif offset == self.REG_CANFD_ID:
+            return self.canfd_id
+        return 0
+        
+    def write(self, offset, value):
+        if offset == self.REG_CONTROL:
+            self.control = value
+            self._update_motor_state()
+        elif offset == self.REG_SPEED_SET:
+            self.speed_setpoint = value
+            self._update_motor_state()
+        elif offset == self.REG_CANFD_ID:
+            self.canfd_id = value
+            
+    def _update_motor_state(self):
+        """Simulate motor response to control inputs"""
+        if self.control & self.CTRL_ENABLE:
+            # Motor enabled - ramp to setpoint
+            self.status |= self.STATUS_RUNNING
+            # Simulate motor reaching speed with some delay
+            if self.actual_speed < self.speed_setpoint:
+                self.actual_speed = min(self.speed_setpoint, self.actual_speed + 100)
+            elif self.actual_speed > self.speed_setpoint:
+                self.actual_speed = max(self.speed_setpoint, self.actual_speed - 100)
+            # Update position based on speed
+            self.position += self.actual_speed // 60  # ticks per update
+            # Simulate current draw
+            self.current = abs(self.actual_speed) * 10  # mA
+        else:
+            # Motor disabled
+            self.status &= ~self.STATUS_RUNNING
+            if self.control & self.CTRL_BRAKE:
+                self.actual_speed = 0
+            else:
+                # Coast down
+                if self.actual_speed > 0:
+                    self.actual_speed -= 50
+                elif self.actual_speed < 0:
+                    self.actual_speed += 50
+            self.current = 0
+
+
+# Renode peripheral interface
+motor = MotorModel()
+
+def read(offset):
+    return motor.read(offset)
+
+def write(offset, value):
+    motor.write(offset, value)
---
+++ simulation/renode/turret_model.py
@@ -0,0 +1,140 @@
+"""
+Turret Model for Renode Simulation
+Simulates pan/tilt turret with servo motors
+"""
+
+class TurretModel:
+    """
+    Pan/Tilt turret simulation model.
+    Controls two servo motors for horizontal (pan) and vertical (tilt) movement.
+    """
+    
+    # Register offsets
+    REG_CONTROL = 0x00        # Control register
+    REG_PAN_SETPOINT = 0x04   # Pan angle setpoint (degrees * 100)
+    REG_PAN_ACTUAL = 0x08     # Actual pan angle (degrees * 100)
+    REG_TILT_SETPOINT = 0x0C  # Tilt angle setpoint (degrees * 100)
+    REG_TILT_ACTUAL = 0x10    # Actual tilt angle (degrees * 100)
+    REG_PAN_SPEED = 0x14      # Pan speed (degrees/sec)
+    REG_TILT_SPEED = 0x18     # Tilt speed (degrees/sec)
+    REG_STATUS = 0x1C         # Status register
+    REG_CANFD_ID = 0x20       # CAN-FD node ID
+    
+    # Control bits
+    CTRL_ENABLE = 0x01
+    CTRL_HOME = 0x02
+    CTRL_TRACK_MODE = 0x04
+    
+    # Status bits
+    STATUS_MOVING = 0x01
+    STATUS_AT_LIMIT = 0x02
+    STATUS_HOMED = 0x04
+    STATUS_FAULT = 0x08
+    
+    # Limits (degrees * 100)
+    PAN_MIN = -18000   # -180 degrees
+    PAN_MAX = 18000    # +180 degrees
+    TILT_MIN = -4500   # -45 degrees
+    TILT_MAX = 9000    # +90 degrees
+    
+    def __init__(self):
+        self.control = 0
+        self.pan_setpoint = 0
+        self.pan_actual = 0
+        self.tilt_setpoint = 0
+        self.tilt_actual = 0
+        self.pan_speed = 3000    # 30 deg/sec default
+        self.tilt_speed = 2000   # 20 deg/sec default
+        self.status = 0
+        self.canfd_id = 0x200
+        
+    def read(self, offset):
+        if offset == self.REG_CONTROL:
+            return self.control
+        elif offset == self.REG_PAN_SETPOINT:
+            return self.pan_setpoint & 0xFFFFFFFF
+        elif offset == self.REG_PAN_ACTUAL:
+            return self.pan_actual & 0xFFFFFFFF
+        elif offset == self.REG_TILT_SETPOINT:
+            return self.tilt_setpoint & 0xFFFFFFFF
+        elif offset == self.REG_TILT_ACTUAL:
+            return self.tilt_actual & 0xFFFFFFFF
+        elif offset == self.REG_PAN_SPEED:
+            return self.pan_speed
+        elif offset == self.REG_TILT_SPEED:
+            return self.tilt_speed
+        elif offset == self.REG_STATUS:
+            return self.status
+        elif offset == self.REG_CANFD_ID:
+            return self.canfd_id
+        return 0
+        
+    def write(self, offset, value):
+        if offset == self.REG_CONTROL:
+            self.control = value
+            if value & self.CTRL_HOME:
+                self._home()
+        elif offset == self.REG_PAN_SETPOINT:
+            # Handle signed value
+            if value > 0x7FFFFFFF:
+                value = value - 0x100000000
+            self.pan_setpoint = max(self.PAN_MIN, min(self.PAN_MAX, value))
+        elif offset == self.REG_TILT_SETPOINT:
+            if value > 0x7FFFFFFF:
+                value = value - 0x100000000
+            self.tilt_setpoint = max(self.TILT_MIN, min(self.TILT_MAX, value))
+        elif offset == self.REG_PAN_SPEED:
+            self.pan_speed = value
+        elif offset == self.REG_TILT_SPEED:
+            self.tilt_speed = value
+        elif offset == self.REG_CANFD_ID:
+            self.canfd_id = value
+            
+    def _home(self):
+        """Home the turret to center position"""
+        self.pan_setpoint = 0
+        self.tilt_setpoint = 0
+        self.pan_actual = 0
+        self.tilt_actual = 0
+        self.status = self.STATUS_HOMED  # Clear other status flags when homing
+        
+    def update(self):
+        """Update turret position based on setpoints"""
+        if not (self.control & self.CTRL_ENABLE):
+            return
+            
+        # Update pan position
+        if self.pan_actual < self.pan_setpoint:
+            self.pan_actual = min(self.pan_setpoint, self.pan_actual + self.pan_speed // 100)
+        elif self.pan_actual > self.pan_setpoint:
+            self.pan_actual = max(self.pan_setpoint, self.pan_actual - self.pan_speed // 100)
+            
+        # Update tilt position
+        if self.tilt_actual < self.tilt_setpoint:
+            self.tilt_actual = min(self.tilt_setpoint, self.tilt_actual + self.tilt_speed // 100)
+        elif self.tilt_actual > self.tilt_setpoint:
+            self.tilt_actual = max(self.tilt_setpoint, self.tilt_actual - self.tilt_speed // 100)
+            
+        # Update status
+        if self.pan_actual != self.pan_setpoint or self.tilt_actual != self.tilt_setpoint:
+            self.status |= self.STATUS_MOVING
+        else:
+            self.status &= ~self.STATUS_MOVING
+            
+        # Check limits
+        if (self.pan_actual == self.PAN_MIN or self.pan_actual == self.PAN_MAX or
+            self.tilt_actual == self.TILT_MIN or self.tilt_actual == self.TILT_MAX):
+            self.status |= self.STATUS_AT_LIMIT
+        else:
+            self.status &= ~self.STATUS_AT_LIMIT
+
+
+# Renode peripheral interface
+turret = TurretModel()
+
+def read(offset):
+    turret.update()
+    return turret.read(offset)
+
+def write(offset, value):
+    turret.write(offset, value)
